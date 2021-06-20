bbb014cc0272887de4eb068af8764c9e49534064
Lachlan Ross <lachlan.f.ross@gmail.com>
2021-06-20 22:28:43 +0800

    Update telebagger.py
    Gets all dialogue on launch now, this is to make sure everything chat / user can be accessed immediately

--- telebagger.py
+++ telebagger.py
@@ -14,6 +14,7 @@ def StartTelegramForwarding():
     api_hash = '19f6d3c9d8d4e6540bce79c3b9223fbe'
     stringsesh = '1BVtsOIIBuziBvZO_VwaerS4NJoP1vAyxIiLLYPdQNn2lXhRFMH2GY_ayqJEM-ax5I-GD4-Kk_lCPjXBTxpmyaDO-hKYeYSyjNYYZ3riEVwFnsy4PGwrXxRwdiKZrICrcEKih0yoTO7lVDO8APaiDpx3E2pFDXyYRhd66Rnj0UyDEcnHPRstadaNTN34BBtLtl7T3LwxQn58ka8sZVRdKAscBjLwiwj8IF4Lwu0oHAH20Ozd5mh8ior82nBz1MRha_C-o6ehdPwhSHFBCCzBqw_zcJ7RjNeeWS4BZ2Jn6XlHatuTXctaMieFymtJkCtEK2gJj9eaf9v5EZGoc1vp9liZ_ra7A7pA='
     client = TelegramClient(StringSession(stringsesh), api_id, api_hash)
+    dialogue = client.get_dialogs()
     
     @client.on(events.NewMessage()) 
     async def my_event_handler(event):
