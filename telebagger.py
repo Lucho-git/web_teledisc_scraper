bd40f9e2e53c57d261a596c6d0799ba3f1cb7daa
Lachlan Ross <lachlan.f.ross@gmail.com>
2021-06-21 13:00:01 +0800

    only @everyone on signal calls,

--- telebagger.py
+++ telebagger.py
@@ -4,7 +4,8 @@ import requests
 
 
 def SendMessageToAlwaysWin(message):
-    message= "<@&834911692303237172>\n" + message
+    if '/USDT' in message:
+        message= "<@&834911692303237172>\n" + message
     mUrl = "https://ptb.discord.com/api/webhooks/838079506660851762/7-lpGNlqWGGlO08XZJ3RwAvSXpWGDf5J6Z4ro5bsdtogYGGXovVfmYGmCb3Jvr1RvtWG"
     data = {"content": message}
     response = requests.post(mUrl, json=data)
